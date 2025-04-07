e42f9a5d0dc896fe6db1e9f1f3bcf134bb0c459a
Nyks <redgo3666@gmail.com>
2025-04-07 13:20:13 +0800

    Changed 'put' to 'Enter' for better grammar; added a case where if the user inputs not (a,b,c, or d) it asks the user again for the input;

--- quiz_create.py
+++ quiz_create.py
@@ -16,13 +16,17 @@
 #Pag di ko masundan matic magtatanong ako. Umamin kyo kagad.
 #Create a demo, send the link of your demo to my messenger before April 12.
 #Upload the source code to your github account using gitbash.
+
 while True:
     question_input = input("Enter your question or type 'exit' to finish: ")
     if question_input == 'exit':
         break
     answers = {}
-    answers['a'] = input("Put answer a: ")
-    answers['b'] = input("Put answer b: ")
-    answers['c'] = input("Put answer c: ")
-    answers['d'] = input("Put answer d: ")
-    correct_answer = input("Enter the correct answer using a,b,c, or d: ")
+    answers['a'] = input("Enter answer a: ")
+    answers['b'] = input("Enter answer b: ")
+    answers['c'] = input("Enter answer c: ")
+    answers['d'] = input("Enter answer d: ")
+    correct_answer = input("Enter the correct answer using (a,b,c, or d): ")
+    while correct_answer not in ['a','b','c','d']:
+        print("Invalid input! Enter the correct answer (a,b,c, or d)")
+        correct_answer = input("Enter the correct answer using (a,b,c, or d): ")
