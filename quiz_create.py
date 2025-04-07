f7b356abfa3b842d1881631b2c965042e66db875
Nyks <redgo3666@gmail.com>
2025-04-07 11:01:27 +0800

    Initilized a repository; Created a new file; Placed the instructions and guidelines.

---
+++ quiz_create.py
@@ -0,0 +1,18 @@
+##For Assignment 9: Quiz Creator
+#
+##Create a program that ask user for a question, it will also ask for 4 possible answer (a,b,c,d) and the correct answer. Write the collected data to a text file. Ask another question until the user chose to exit.
+#
+#Hint: 
+#On the next assignment, you will create the Quiz program that read the output file of the Quiz Creator. The user will answer the randomly selected question and check if the answer is correct.
+#Format the data written on the text file so it is easier the read on the next assignment.
+#Use your creativity, dapat maangas.
+#Feel free to use any library
+#
+#
+#Important Note:
+#Bawal ng mangopya. Matindi ang kaparusahan.
+#Give descriptive caption s mga commit. Match dapat s last code changes.
+#Kaylangan ko masundan kng paano nabuo ung program using your commits.
+#Pag di ko masundan matic magtatanong ako. Umamin kyo kagad.
+#Create a demo, send the link of your demo to my messenger before April 12.
+#Upload the source code to your github account using gitbash.
