97e4be7a327531843f854ed766917696d7316ab1
Nyks <redgo3666@gmail.com>
2025-05-03 12:26:47 +0800

    Add a loop that will iterate 4 lines of the choices and extract the letter and answers in a dictionary

--- quiz_main.py
+++ quiz_main.py
@@ -11,3 +11,8 @@ def read_quiz(quiz_file):
         questions = []
         while True:
             question = file.readline().strip().removeprefix("Question: ")
+            choices = {}
+            for i in range(4):
+                choice_line = file.readline().strip()
+                letter, answer = choice_line.split(".")
+                choices[letter.strip] = answer.strip
